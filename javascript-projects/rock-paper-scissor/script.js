4eca0ee498bd154e9264990bda624e5a3ea41ec9
DaemNE <Nikolaasdaem@hotmail.com>
2022-09-06 11:12:41 +0200

    Updated UI rps-game

--- javascript-projects/rock-paper-scissor/script.js
+++ javascript-projects/rock-paper-scissor/script.js
@@ -1,13 +1,12 @@
 const CHOICES = ["rock", "paper", "scissors"]
 let computerSelection = ""
 let playerSelection = ""
-let playerDisplay = document.querySelector(".playerWins")
-let computerDisplay = document.querySelector(".computerWins")
-let outcomeDisplay = document.querySelector(".outcome")
+let playerDisplay = document.getElementById("playerScore")
+let computerDisplay = document.getElementById("computerScore")
+let outcomeDisplay = document.getElementById("outcome")
 let computerWins = 0
 let playerWins = 0
 let maxWins = 0
-let result
 
 function getComputerSelection() {
     computerSelection = CHOICES[Math.floor(Math.random()*CHOICES.length)]
@@ -15,104 +14,65 @@ function getComputerSelection() {
     return computerSelection
 }
 
-function startGame() {
-    if (maxWins) {
-        for (let i = 0 ; i < 10; i++) {
-            let checkValidity = false
-            while (!checkValidity) {
-                let playerSelection = prompt("Choose rock, paper or scissors", "")
-                if (!(playerSelection === null)){
-                    playerSelection = playerSelection.toLowerCase()
-                    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-                        getComputerSelection()
-                        checkOutcome(playerSelection, computerSelection)
-                        checkValidity = true
-                        updateDisplay()
-                    } 
-                } else if (playerSelection === null){
-                    i += 100;
-                    resetGame()
-                    break;
-                }
-            }
-            if (playerWins == maxWins || computerWins == maxWins) {
-                break;
-            }
-        }
-    } else {
-        alert("You need to pick a gamemode!")
-    }
-}
-
 function resetGame() {
     computerWins = 0
     playerWins = 0
-    maxWins = 0
-    document.getElementById("buttonSetToOne").style.background='white'
-    document.getElementById("buttonSetToThree").style.background='white'
-    document.getElementById("buttonSetToFive").style.background='white'
-    updateDisplay()
-    outcomeDisplay.textContent = "Click Start game to start!"
-}
-
-function checkOutcome(playerChoice, computerChoice) {
-
-    if(playerChoice === "rock") {
-        if(computerChoice === "rock") {
-            result = "It's a draw!"
-        } else if (computerChoice === "paper"){
-            result = "You lost, the computer wins!"
-            computerWins++
-        } else if (computerChoice === "scissors"){
-            result = "You win, the computer loses!"
-            playerWins++
-        }
-    } else if (playerChoice === "paper") {
-        if(computerChoice === "paper") {
-            result = "It's a draw!"
-        } else if (computerChoice === "scissors"){
-            result = "You lost, the computer wins!"
-            computerWins++
-        } else if (computerChoice === "paper"){
-            result = "You win, the computer loses!"
-            playerWins++
-        }
-        
-    } else if (playerChoice === "scissors"){
-        if(computerChoice === "scissors") {
-            result = "It's a draw!"
-        } else if (computerChoice === "rock"){
-            result = "You lost, the computer wins!"
-            computerWins++
-        } else if (computerChoice === "paper"){
-            result = "You win, the computer loses!"
-            playerWins++
-        }
-    }
-    console.log(result)
-}
-
-function setFirstToOne() {
-    maxWins = 1;
-    document.getElementById("buttonSetToOne").style.background='green'
-    document.getElementById("buttonSetToThree").style.background='white'
-    document.getElementById("buttonSetToFive").style.background='white'
-}
-function setFirstToThree() {
-    maxWins = 3;
-    document.getElementById("buttonSetToOne").style.background='white'
-    document.getElementById("buttonSetToThree").style.background='green'
-    document.getElementById("buttonSetToFive").style.background='white'
-}
-function setFirstToFive() {
-    maxWins = 5;
-    document.getElementById("buttonSetToOne").style.background='white'
-    document.getElementById("buttonSetToThree").style.background='white'
-    document.getElementById("buttonSetToFive").style.background='green'
+    document.getElementById('playerScore').textContent = "Playerscore = 0"
+    document.getElementById('computerScore').textContent = "Computerscore = 0"
 }
 
 function updateDisplay(){
-    outcomeDisplay.textContent = result
-    playerDisplay.textContent = `The player won ${playerWins} times`
-    computerDisplay.textContent = `The computer won ${computerWins} times`
+    playerDisplay.textContent = `Playerscore = ${playerWins}`
+    computerDisplay.textContent = `Computerscore =  ${computerWins}`
+}
+
+function rock(){
+    let computerChoice = getComputerSelection();
+    if (computerChoice === "scissors") {
+        outcomeDisplay.textContent = "You won!";
+        outcomeDisplay.style.backgroundColor = 'green' 
+        playerWins++;
+    } else if (computerChoice === "rock") {
+        outcomeDisplay.textContent = "It's a draw!";
+        outcomeDisplay.style.backgroundColor = 'grey' 
+    } else {
+        outcomeDisplay.textContent = "You lost!";
+        outcomeDisplay.style.backgroundColor = 'red' 
+        computerWins++
+    }
+    updateDisplay()
+}
+
+function paper(){
+    let computerChoice = getComputerSelection();
+    if (computerChoice === "scissors") {
+        outcomeDisplay.textContent = "You lost!";
+        outcomeDisplay.style.backgroundColor = 'red' 
+        computerWins++;
+    } else if (computerChoice === "paper") {
+        outcomeDisplay.textContent = "It's a draw!";
+        outcomeDisplay.style.backgroundColor = 'grey' 
+    } else {
+        outcomeDisplay.textContent = "You won!";
+        outcomeDisplay.style.backgroundColor = 'green' 
+        playerWins++
+    }
+    updateDisplay()
+}
+
+function scissor(){
+    let computerChoice = getComputerSelection();
+    if (computerChoice === "paper") {
+        outcomeDisplay.textContent = "You lost!";
+        outcomeDisplay.style.backgroundColor = 'red' 
+        computerWins++;
+    } else if (computerChoice === "scissors") {
+        outcomeDisplay.textContent = "It's a draw!";
+        outcomeDisplay.style.backgroundColor = 'grey' 
+    } else {
+        outcomeDisplay.textContent = "You won!";
+        outcomeDisplay.style.backgroundColor = 'green' 
+        playerWins++
+    }
+    updateDisplay()
 }
