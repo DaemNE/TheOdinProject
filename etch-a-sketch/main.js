b3128bc25a487ef1b3fbfb72bc480ee63439a388
DaemNE <Nikolaasdaem@hotmail.com>
2022-09-06 14:47:52 +0200

    Added extra functionality to Etch-a-Sketch

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,26 +1,44 @@
 const container = document.getElementById("container");
 let resolution = 16;
 let color = "white"
+let method = "mouseover";
 document.getElementById("setWhiteButton").style.backgroundColor = "green"
 document.getElementById("set16x16").style.backgroundColor = "green"
+document.getElementById("onHoverButton").style.backgroundColor = "green"
 
 function setWhite() {
     color = "white"
     document.getElementById("setWhiteButton").style.backgroundColor = "green"
     document.getElementById("setBlueButton").style.backgroundColor = "white"
     document.getElementById("setYellowButton").style.backgroundColor = "white"
+    document.getElementById("setRainbowButton").style.backgroundColor = "white"
+
 }
+
 function setBlue() {
     color = "blue"
     document.getElementById("setWhiteButton").style.backgroundColor = "white"
     document.getElementById("setBlueButton").style.backgroundColor = "green"
     document.getElementById("setYellowButton").style.backgroundColor = "white"
+    document.getElementById("setRainbowButton").style.backgroundColor = "white"
+
 }
+
 function setYellow() {
     color = "yellow"
     document.getElementById("setWhiteButton").style.backgroundColor = "white"
     document.getElementById("setBlueButton").style.backgroundColor = "white"
     document.getElementById("setYellowButton").style.backgroundColor = "green"
+    document.getElementById("setRainbowButton").style.backgroundColor = "white"
+
+}
+
+function setRainbow() {
+    color = "rainbow"
+    document.getElementById("setWhiteButton").style.backgroundColor = "white"
+    document.getElementById("setBlueButton").style.backgroundColor = "white"
+    document.getElementById("setYellowButton").style.backgroundColor = "white"
+    document.getElementById("setRainbowButton").style.backgroundColor = "green"
 }
 
 function setResolution256(){
@@ -29,7 +47,7 @@ function setResolution256(){
     document.getElementById("set16x16").style.backgroundColor = "green"
     document.getElementById("set32x32").style.backgroundColor = "white"
     document.getElementById("set64x64").style.backgroundColor = "white"
-    makeRows(resolution)
+    makeRows(resolution, method)
 }
 
 function setResolution1024(){
@@ -38,7 +56,7 @@ function setResolution1024(){
     document.getElementById("set16x16").style.backgroundColor = "white"
     document.getElementById("set32x32").style.backgroundColor = "green"
     document.getElementById("set64x64").style.backgroundColor = "white"
-    makeRows(resolution)
+    makeRows(resolution, method)
 }
 
 function setResolution4096(){
@@ -47,19 +65,53 @@ function setResolution4096(){
     document.getElementById("set16x16").style.backgroundColor = "white"
     document.getElementById("set32x32").style.backgroundColor = "white"
     document.getElementById("set64x64").style.backgroundColor = "green"
-    makeRows(resolution)
+    makeRows(resolution, method)
+}
+
+function setMethodHover() {
+    method = "mouseover"
+    updateGrid()
+    makeRows(resolution, method)
+    document.getElementById("onHoverButton").style.backgroundColor = "green"
+    document.getElementById("onClickButton").style.backgroundColor = "white"
+}
+function setMethodOnClick() {
+    method = "click"
+    updateGrid()
+    makeRows(resolution, method)
+    document.getElementById("onHoverButton").style.backgroundColor = "white"
+    document.getElementById("onClickButton").style.backgroundColor = "green"
 }
 
 
-
-function makeRows(resolution) {
+function makeRows(resolution, method) {
     container.style.setProperty('--grid-rows', resolution);
     container.style.setProperty('--grid-cols', resolution);
     for (let i = 0; i < (resolution * resolution); i++) {
         let cell = document.createElement("div");
         
-        cell.addEventListener("mouseover", (e) => {
+        cell.addEventListener(method, (e) => {
             
+            if (color === "rainbow") {
+                let randomNumber = Math.floor(Math.random()*7)+1
+
+                switch (randomNumber) {
+                    case 1: cell.style.backgroundColor = "red";
+                    break;
+                    case 2: cell.style.backgroundColor = "orange";
+                    break;
+                    case 3: cell.style.backgroundColor = "yellow";
+                    break;
+                    case 4: cell.style.backgroundColor = "green";
+                    break;
+                    case 5: cell.style.backgroundColor = "blue";
+                    break;
+                    case 6: cell.style.backgroundColor = "indigo";
+                    break;
+                    case 7: cell.style.backgroundColor = "violet";
+                    break;
+                }
+            }
             cell.style.backgroundColor = color;
         }
         )
@@ -91,4 +143,4 @@ function clearCanvas() {
 
 
 
-makeRows(resolution,resolution)
+makeRows(resolution, method)
