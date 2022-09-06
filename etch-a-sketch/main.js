4ac6e56ecd4df924739bfb6bcaf3904aec771656
DaemNE <Nikolaasdaem@hotmail.com>
2022-09-06 17:16:35 +0200

    Added new features to Etch-a-Sketch project

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -2,9 +2,15 @@ const container = document.getElementById("container");
 let resolution = 16;
 let color = "white"
 let method = "mouseover";
+let background = "white";
+let luckyState = false;
+let luckyColor = "white"
+
 document.getElementById("setWhiteButton").style.backgroundColor = "green"
 document.getElementById("set16x16").style.backgroundColor = "green"
 document.getElementById("onHoverButton").style.backgroundColor = "green"
+document.getElementById("setCanvasWhite").style.backgroundColor = "green"
+document.getElementById("toggleLuckyButton").style.backgroundColor = "red"
 
 function setWhite() {
     color = "white"
@@ -47,7 +53,7 @@ function setResolution256(){
     document.getElementById("set16x16").style.backgroundColor = "green"
     document.getElementById("set32x32").style.backgroundColor = "white"
     document.getElementById("set64x64").style.backgroundColor = "white"
-    makeRows(resolution, method)
+    makeRows(resolution)
 }
 
 function setResolution1024(){
@@ -56,7 +62,7 @@ function setResolution1024(){
     document.getElementById("set16x16").style.backgroundColor = "white"
     document.getElementById("set32x32").style.backgroundColor = "green"
     document.getElementById("set64x64").style.backgroundColor = "white"
-    makeRows(resolution, method)
+    makeRows(resolution)
 }
 
 function setResolution4096(){
@@ -65,39 +71,107 @@ function setResolution4096(){
     document.getElementById("set16x16").style.backgroundColor = "white"
     document.getElementById("set32x32").style.backgroundColor = "white"
     document.getElementById("set64x64").style.backgroundColor = "green"
-    makeRows(resolution, method)
+    makeRows(resolution)
 }
 
 function setMethodHover() {
     method = "mouseover"
     updateGrid()
-    makeRows(resolution, method)
+    makeRows(resolution)
     document.getElementById("onHoverButton").style.backgroundColor = "green"
     document.getElementById("onClickButton").style.backgroundColor = "white"
 }
 function setMethodOnClick() {
     method = "click"
     updateGrid()
-    makeRows(resolution, method)
+    makeRows(resolution)
     document.getElementById("onHoverButton").style.backgroundColor = "white"
     document.getElementById("onClickButton").style.backgroundColor = "green"
 }
 
+function setCanvasWhite() {
+    background = "white"
+    luckyColor = "white"
+    updateGrid()
+    makeRows(resolution)
+    document.getElementById("setCanvasWhite").style.backgroundColor = "green"
+    document.getElementById("setCanvasBlack").style.backgroundColor = "white"
+    document.getElementById("setCanvasRed").style.backgroundColor = "white"
+    document.getElementById("setCanvasBlue").style.backgroundColor = "white"
+}
+function setCanvasBlack() {
+    background = "black"
+    luckyColor = "black"
+    updateGrid()
+    makeRows(resolution)
+    document.getElementById("setCanvasWhite").style.backgroundColor = "white"
+    document.getElementById("setCanvasBlack").style.backgroundColor = "green"
+    document.getElementById("setCanvasRed").style.backgroundColor = "white"
+    document.getElementById("setCanvasBlue").style.backgroundColor = "white"
+    
+}
+function setCanvasRed() {
+    background = "red"
+    luckyColor = "red"
+    updateGrid()
+    makeRows(resolution)
+    document.getElementById("setCanvasWhite").style.backgroundColor = "white"
+    document.getElementById("setCanvasBlack").style.backgroundColor = "white"
+    document.getElementById("setCanvasRed").style.backgroundColor = "green"
+    document.getElementById("setCanvasBlue").style.backgroundColor = "white"
+}
+function setCanvasBlue() {
+    background = "blue"
+    luckyColor = "blue"
+    updateGrid()
+    makeRows(resolution)
+    document.getElementById("setCanvasWhite").style.backgroundColor = "white"
+    document.getElementById("setCanvasBlack").style.backgroundColor = "white"
+    document.getElementById("setCanvasRed").style.backgroundColor = "white"
+    document.getElementById("setCanvasBlue").style.backgroundColor = "green"
+}
 
-function makeRows(resolution, method) {
+function toggleLucky() {
+    if (luckyState) {
+        luckyState = false
+        document.getElementById("toggleLuckyButton").style.backgroundColor = "red"
+    } else {
+        luckyState = true
+        document.getElementById("toggleLuckyButton").style.backgroundColor = "green"
+    }
+    updateGrid()
+    makeRows(resolution)
+
+}
+
+function makeRows(resolution) {
     container.style.setProperty('--grid-rows', resolution);
     container.style.setProperty('--grid-cols', resolution);
     let count = 0
     for (let i = 0; i < (resolution * resolution); i++) {
         let cell = document.createElement("div");
         let luckyNumber = Math.floor(Math.random()*500);
-        
-        if (luckyNumber === 1 && count < 1 ) {
-            
-            cell.textContent = "*"
-            cell.style.color = "white"
+
+        if (resolution === 16) {
             cell.style.fontSize = "x-large";
+        } else if (resolution === 32) {
+            cell.style.fontSize = "medium";
+        } else {
+            cell.style.fontSize = "xx-small";
+        }
+
+        cell.style.color = luckyColor
+        cell.style.backgroundColor = background;
+
+        if (luckyNumber === 1 && count < 1 && luckyState) {
+            console.log("X marked")
+            cell.textContent = "*"
+            document.getElementById("isLucky").innerText = "We've placed a gift"
             count++;
+        }
+
+        if (background === "black") {
+            cell.style.borderColor = "white"
         }
         cell.addEventListener(method, (e) => {
             
@@ -121,6 +195,13 @@ function makeRows(resolution, method) {
                     break;
                 }
             }
+
+            if (background === "black" && color === "white") {
+                cell.style.borderColor = "black"
+            }
+            if (background === "white" && color === "black") {
+                cell.style.borderColor = "white"
+            }
             cell.style.backgroundColor = color;
         }
         )
@@ -131,7 +212,6 @@ function makeRows(resolution, method) {
 
 function updateGrid() {
     let grid = document.getElementById("container")
-    console.log(resolution)
     for (let i = 0; i < resolution * resolution; i++) {
         grid.removeChild(document.querySelector("#container .grid-item"))
     }
@@ -144,12 +224,4 @@ function clearCanvas() {
     }
 }
 
-
-
-
-
-
-
-
-
-makeRows(resolution, method)
+makeRows(resolution)
