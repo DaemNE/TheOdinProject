67719fc84c64ab4689b4cb1861962212c6c5b940
DaemNE <Nikolaasdaem@hotmail.com>
2022-09-08 21:12:35 +0200

    Added css foundations to calculator

---
+++ javascript-projects/calculator/main.js
@@ -0,0 +1,8 @@
+const screenTop = document.getElementById("screenTop")
+const screenBottom = document.getElementById("screenBottom")
+
+screenTop.textContent = "hi"
+
+function add(a, b) {
+    return a + b;
+}
