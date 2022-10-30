e9d826e5b3f0f987138f1930467546843553fed4
DaemNE <Nikolaasdaem@hotmail.com>
2022-10-30 21:55:44 +0100

    Finished webpack npm guide

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,23 @@
 import _ from 'lodash';
+import printMe from './print.js';
+import myName from './myName.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
+    const myNameBtn = document.createElement('button');
+    const name = "niko"
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    myNameBtn.innerHTML = 'Click to print name';
+    myNameBtn.onclick = myName;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+    element.appendChild(myNameBtn);
 
     return element;
 }
---
+++ webpack-demo/src/myName.js
@@ -0,0 +1,3 @@
+export default function myName() {
+    console.log(`Hi! My name is niko`);
+}
