31b4fee89c4a7e94aee603aa9e36642db48a542a
DaemNE <Nikolaasdaem@hotmail.com>
2022-12-08 20:09:15 +0100

    React Hooks progression 70/118

--- the-odin-project-reactJs/react-hooks/src/App.js
+++ the-odin-project-reactJs/react-hooks/src/App.js
@@ -1,12 +1,39 @@
+import React from 'react';
 import './App.css';
-import IntervalClassCounter from './components/IntervalClassCounter';
-import IntervalHookCounter from './components/IntervalHookCounter';
+import ParentComponent from './components/ParentComponent';
+/* import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
 
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+    switch(action) {
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        case 'reset':
+            return initialState;
+        default:
+            return state
+    }
+} */
+/* <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
+      <div className="App">
+      Count - {count}
+      <ComponentA />
+      <ComponentB />
+      <ComponentC />
+      </div>
+    </CountContext.Provider> */
 function App() {
+  /* const [count, dispatch] = useReducer(reducer, initialState) */
   return (
-    <div className="App">
-      <IntervalClassCounter />
-      <IntervalHookCounter />
+    
+    <div className='App'>
+      <ParentComponent />
     </div>
   );
 }
