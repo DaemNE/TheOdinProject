38fc2979f93b8fda908e99e2e27547222d62415c
DaemNE <Nikolaasdaem@hotmail.com>
2022-12-15 22:27:58 +0100

    tests!

--- the-odin-project-reactJs/react-testing/src/components/counter/Counter.test.tsx
+++ the-odin-project-reactJs/react-testing/src/components/counter/Counter.test.tsx
@@ -1,5 +1,6 @@
 import {render, screen} from '@testing-library/react'
 import Counter from './Counter'
+import user from '@testing-library/user-event'
 
 
 describe("Counter", ()=> {
@@ -12,4 +13,62 @@ describe("Counter", ()=> {
         })
         expect(incrementButton).toBeInTheDocument()
     })
+
+    test('renders a count of 0', () => {
+        render(<Counter />)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent("0")
+    })
+
+    test('renders a count of 1 after clicking the increment button', async () => {
+        user.setup()
+        render(<Counter/>)
+        const incrementButton = screen.getByRole('button', {
+            name: 'Increment'
+        })
+        await user.click(incrementButton)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent("1") 
+
+    })
+
+    test('renders a count of 2 after clicking the increment button twice', async () => {
+        user.setup()
+        render(<Counter />)
+        const incrementButton = screen.getByRole('button', {
+            name: 'Increment'
+        })
+        await user.click(incrementButton)
+        await user.click(incrementButton)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent("2") 
+    })
+
+    test("renders a count of 10 after clicking the set button", async () => {
+        user.setup()
+        render(<Counter />)
+        const setButton = screen.getByRole('button', {
+            name: 'Set'
+        })
+        const inputElement = screen.getByRole('spinbutton')
+        await user.type(inputElement, "10")
+        expect(inputElement).toHaveValue(10)
+        await user.click(setButton)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent("10")
+    })
+
+    test("elements are focussed in the right order", async () => {
+        user.setup()
+        render(<Counter />)
+        const amountInput = screen.getByRole('spinbutton')
+        const setButton = screen.getByRole('button', {name:"Set"})
+        const incrementButton = screen.getByRole('button', {name:"Increment"})
+        await user.tab()
+        expect(incrementButton).toHaveFocus()
+        await user.tab()
+        expect(amountInput).toHaveFocus()
+        await user.tab()
+        expect(setButton).toHaveFocus()
+    }) 
 })
