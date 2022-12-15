70a81a086eb7e5ace6c7e19f359e12de3ec7ac33
DaemNE <Nikolaasdaem@hotmail.com>
2022-12-15 15:15:56 +0100

    Added greet component and tests

---
+++ the-odin-project-reactJs/react-testing/src/components/greet/Greet.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type GreetProps = {
+    name?: string
+}
+
+export default function Greet({name}: GreetProps) {
+  return (
+    <div>
+      Hello {name}
+    </div>
+  )
+}
