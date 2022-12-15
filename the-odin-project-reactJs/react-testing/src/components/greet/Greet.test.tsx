8ec41a0c20e56e812ddeb3b5bec1c1bbf24a56cc
DaemNE <Nikolaasdaem@hotmail.com>
2022-12-15 15:57:13 +0100

    Added more tests

--- the-odin-project-reactJs/react-testing/src/components/greet/Greet.test.tsx
+++ the-odin-project-reactJs/react-testing/src/components/greet/Greet.test.tsx
@@ -6,14 +6,19 @@ it should render  hello followed by the name
 import { render, screen } from "@testing-library/react"
 import Greet from "./Greet"
 
-test("Greet renders correctly", () => {
-    render(<Greet />)
-    const textElement = screen.getByText("Hello")
-    expect(textElement).toBeInTheDocument()
-})
-
-test('Greet renders with a name', () => {
-    render(<Greet name="Vishwas"/>)
-    const textElement = screen.getByText("Hello Vishwas")
-    expect(textElement).toBeInTheDocument()
+describe('Greet', () => {
+    test("Renders correctly", () => {
+        render(<Greet />)
+        const textElement = screen.getByText("Hello")
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    describe ("Nested", () => {
+        test('Renders with a name', () => {
+            render(<Greet name="Vishwas"/>)
+            const textElement = screen.getByText("Hello Vishwas")
+            expect(textElement).toBeInTheDocument()
+        })
+    })
+    
 })
