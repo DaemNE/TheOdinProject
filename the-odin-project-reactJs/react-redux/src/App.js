cc63d2e62567b5f7eaf20578f6b85b3a92da0c93
DaemNE <Nikolaasdaem@hotmail.com>
2022-12-23 12:07:59 +0100

    added mapDispatchToProps and mapStateToProps with ownProps

--- the-odin-project-reactJs/react-redux/src/App.js
+++ the-odin-project-reactJs/react-redux/src/App.js
@@ -5,15 +5,17 @@ import store from "./redux/store";
 import IceCreamContainer from "./components/IceCreamContainer";
 import HooksCookieContainer from "./components/HooksCookieContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <CakeContainer />
         <IceCreamContainer />
         <HooksCookieContainer />
-        <NewCakeContainer />
       </div>
     </Provider>
   );
