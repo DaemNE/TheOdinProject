83d747bb8d36f38d144b5959709e4af568268897
DaemNE <Nikolaasdaem@hotmail.com>
2022-12-27 20:45:16 +0100

    Added switch button example

---
+++ the-odin-project-reactJs/react-material-ui/src/components/MuiSwitch.tsx
@@ -0,0 +1,24 @@
+import { Box, FormControlLabel, Switch } from "@mui/material";
+import { useState } from "react";
+export const MuiSwitch = () => {
+  const [checked, setChecked] = useState(false);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+  console.log({ checked });
+  return (
+    <Box>
+      <FormControlLabel
+        label="Dark Mode"
+        control={
+          <Switch
+            checked={checked}
+            onChange={handleChange}
+            size="small"
+            color="success"
+          />
+        }
+      ></FormControlLabel>
+    </Box>
+  );
+};
