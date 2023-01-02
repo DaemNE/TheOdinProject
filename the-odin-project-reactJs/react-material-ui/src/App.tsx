f5dee91e52b43b1358e97e5b7cc57d681f97e683
Nikolaas Daem <nikolaasdaem@hotmail.com>
2023-01-02 18:10:36 +0100

    Added masonry example

--- the-odin-project-reactJs/react-material-ui/src/App.tsx
+++ the-odin-project-reactJs/react-material-ui/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import { MuiMasonry } from "./components/MuiMasonry";
 import { MuiTimeline } from "./components/MuiTimeline";
 
 function App() {
   return (
     <div className="App">
-      <MuiTimeline />
+      <MuiMasonry />
     </div>
   );
 }
