3a169b83f3a0fbea41f4ecd6dea61470d9a4ffb0
Nikolaas Daem <nikolaasdaem@hotmail.com>
2023-01-02 17:52:31 +0100

    Added mui tabs example

--- the-odin-project-reactJs/react-material-ui/src/App.tsx
+++ the-odin-project-reactJs/react-material-ui/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { MuiLoadingButton } from "./components/MuiLoadingButton";
+import { MuiTabs } from "./components/MuiTabs";
 
 function App() {
   return (
     <div className="App">
-      <MuiLoadingButton />
+      <MuiTabs />
     </div>
   );
 }
