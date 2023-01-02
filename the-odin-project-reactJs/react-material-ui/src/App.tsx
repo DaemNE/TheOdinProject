da5158975bedc678d8300af2fce553b529d1a75e
Nikolaas Daem <nikolaasdaem@hotmail.com>
2023-01-02 19:02:23 +0100

    Added custom theme and finished material ui guide

--- the-odin-project-reactJs/react-material-ui/src/App.tsx
+++ the-odin-project-reactJs/react-material-ui/src/App.tsx
@@ -1,11 +1,32 @@
 import "./App.css";
 import { MuiResponsiveness } from "./components/MuiResponsiveness";
+import { createTheme, colors, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  status: {
+    danger: "#e53e3e",
+  },
+  palette: {
+    primary: {
+      main: colors.pink[500],
+    },
+    secondary: {
+      main: colors.orange[500],
+    },
+    neutral: {
+      main: colors.grey[500],
+      darker: colors.grey[700],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <MuiResponsiveness />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <MuiResponsiveness />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- the-odin-project-reactJs/react-material-ui/src/components/MuiResponsiveness.tsx
+++ the-odin-project-reactJs/react-material-ui/src/components/MuiResponsiveness.tsx
@@ -1,25 +1,35 @@
 import { Box } from "@mui/material";
+import { styled } from "@mui/material";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  height: "255px",
+  width: "255px",
+  backgroundColor: theme.palette.neutral?.darker,
+}));
 
 export const MuiResponsiveness = () => {
   return (
-    <Box
-      sx={{
-        height: "300px",
-        width: {
-          xs: 100, // 0
-          sm: 200, // 600
-          md: 300, // 900
-          lg: 400, // 1200
-          xl: 500, // 1536
-        },
-        bgcolor: {
-          xs: "primary.main", // 0
-          sm: "primary.light", // 600
-          md: "primary.dark", // 900
-          lg: "secondary.light", // 1200
-          xl: "secondary.main", // 1536
-        },
-      }}
-    ></Box>
+    <>
+      <Box
+        sx={{
+          height: "300px",
+          width: {
+            xs: 100, // 0
+            sm: 200, // 600
+            md: 300, // 900
+            lg: 400, // 1200
+            xl: 500, // 1536
+          },
+          bgcolor: {
+            xs: "primary.main", // 0
+            sm: "primary.light", // 600
+            md: "secondary.main", // 900
+            lg: "secondary.light", // 1200
+            xl: "secondary.dark", // 1536
+          },
+        }}
+      ></Box>
+      <StyledBox />
+    </>
   );
 };
