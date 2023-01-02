2f7072bf8563d73213b56ff3679f927b9146b957
Nikolaas Daem <nikolaasdaem@hotmail.com>
2023-01-02 11:33:57 +0100

    Added drawer and Speeddial example

--- the-odin-project-reactJs/react-material-ui/src/App.tsx
+++ the-odin-project-reactJs/react-material-ui/src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import { MuiBreadcrumbs } from "./components/MuiBreadcrumbs";
+import { MuiDrawer } from "./components/MuiDrawer";
+import { MuiNavbar } from "./components/MuiNavbar";
+import { MuiSpeedDial } from "./components/MuiSpeedDial";
 
 function App() {
   return (
     <div className="App">
+      <MuiNavbar />
       <MuiBreadcrumbs />
+      <MuiDrawer />
+      <MuiSpeedDial />
     </div>
   );
 }
