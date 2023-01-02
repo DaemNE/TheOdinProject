32f19c1a43be2b60e48f3f97a8b30b021d76bbf2
Nikolaas Daem <nikolaasdaem@hotmail.com>
2023-01-02 13:14:59 +0100

    Added muichips example

--- the-odin-project-reactJs/react-material-ui/src/App.tsx
+++ the-odin-project-reactJs/react-material-ui/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { MuiList } from "./components/MuiList";
+import { MuiChip } from "./components/MuiChip";
 
 function App() {
   return (
     <div className="App">
-      <MuiList />
+      <MuiChip />
     </div>
   );
 }
